5f0f0dcdd9e3b4157af96ecf2dbb7b9d5e837fe2
EtherTote <36612007+ethertote@users.noreply.github.com>
2018-03-30 15:18:17 +0100

    Add files via upload

--- contractAddressCalculator.sol
+++ contractAddressCalculator.sol
@@ -1,31 +1,26 @@
 pragma solidity ^0.4.20;
 
 // Ethertote.com solidity contract address calculator
-// used to calculate contract address before being deployed
-// to use simply deploy dummy contract then check its
-// nonce value on Etherscan. Increment the nonce by 1 and supply to
-// this contract to calculate the next contract address
+
+// used to calculate contract addresses before being deployed
+// to use calculator simply deploy the contract and 
+// then supply a nonce value (must be less than 128), followed by a comma, 
+// then the sending address.
+// For example:  5, 0xfa8cc96e2e8304c59285aff8b54a5d5e9112321c
+// which will give you the contract address of the 5th deployed
+// contract sent from address 0xfa8cc96e2e8304c59285aff8b54a5d5e9112321c
+// You can check the last current nonce in Etherscan
 
 contract contractAddressCalculator {
     
-      address public firstDeployment;
-    
-  function addressForNonce(uint8 nonce) constant returns (address) {
-    if (nonce <= 127)
-    return address(sha3(0xd6, 0x94, address(this), nonce));
-	
-	else if (nonce >= 128 && nonce <= 257)
-	return address(sha3(0xd7, 0x94, address(this), 0x81, nonce));
+
+  function addressForNonce(uint8 nonce_value, address acct_addr) constant returns (address) {
+    if (nonce_value <= 127)
+    return address(keccak256(0xd6, 0x94, acct_addr, nonce_value));
 	
 	else throw;
 	
   }
 
-  function contractAddressCalculator() payable {
-    firstDeployment = addressForNonce(uint8(1));
-    bool b = firstDeployment.send(msg.value);
-  }
-  
-  
 
 }
